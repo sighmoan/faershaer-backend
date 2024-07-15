e20d8e30fb87837af3a0a0eeb748858feaf16b5b
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-15 10:28:16 +0200

    initial commit

---
+++ src/main/java/tech/tolpuddle/faershaer_backend/FaershaerBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.tolpuddle.faershaer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaershaerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaershaerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/tolpuddle/faershaer_backend/FaershaerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tolpuddle.faershaer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaershaerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
