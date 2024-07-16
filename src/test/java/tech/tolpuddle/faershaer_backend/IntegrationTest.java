ee9a787a0e153406bba0f22031860861e4b31752
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-16 08:49:16 +0200

    add integration test

---
+++ src/test/java/tech/tolpuddle/faershaer_backend/IntegrationTest.java
@@ -0,0 +1,50 @@
+package tech.tolpuddle.faershaer_backend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasLength;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class IntegrationTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    void shouldListTransactions() {
+        RequestBuilder req = MockMvcRequestBuilders.get("/");
+        try {
+            mockMvc.perform(req)
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.length()", is(10)))
+                    .andExpect(jsonPath("$.[0].id", is("1")))
+                    .andExpect(jsonPath("$.[0].expense", is("Parking")))
+                    .andExpect(jsonPath("$.[0].sum", is(10.0)));
+        } catch(Exception ex) {
+            fail(ex.getMessage());
+        }
+    }
+}
