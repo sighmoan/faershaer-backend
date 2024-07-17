55218631843b74216f2fbd172aeb8e0d14d619ea
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-17 10:18:05 +0200

    rename balance in dto to match frontend (and not overlap)

--- src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/ReimbursementDto.java
+++ src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/ReimbursementDto.java
@@ -2,7 +2,7 @@ package tech.tolpuddle.faershaer_backend.http.dtos;
 
 import tech.tolpuddle.faershaer_backend.domain.Reimbursement;
 
-public record ReimbursementDto (String creditor, String debtor, Double balance) {
+public record ReimbursementDto (String creditor, String debtor, Double amount) {
     static public ReimbursementDto getDto(Reimbursement rb) {
         return new ReimbursementDto(rb.getCreditor().getName(), rb.getDebtor().getName(), rb.getSum());
     }
