cd9cd2eea43eb8820df090d556eca2315a9d0a61
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-18 14:39:06 +0200

    add portrait urls to reimbursements

--- src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/ReimbursementDto.java
+++ src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/ReimbursementDto.java
@@ -2,8 +2,13 @@ package tech.tolpuddle.faershaer_backend.http.dtos;
 
 import tech.tolpuddle.faershaer_backend.domain.Reimbursement;
 
-public record ReimbursementDto (String creditor, String debtor, Double amount) {
+public record ReimbursementDto (String creditor, String creditorPortraitUrl, String debtor, String debtorPortraitUrl, Double amount) {
     static public ReimbursementDto getDto(Reimbursement rb) {
-        return new ReimbursementDto(rb.getCreditor().getName(), rb.getDebtor().getName(), rb.getSum());
+        return new ReimbursementDto(
+                rb.getCreditor().getName(),
+                rb.getCreditor().getPortraitUrl(),
+                rb.getDebtor().getName(),
+                rb.getDebtor().getPortraitUrl(),
+                rb.getSum());
     }
 }
--- src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/TransactionDto.java
+++ src/main/java/tech/tolpuddle/faershaer_backend/http/dtos/TransactionDto.java
@@ -2,8 +2,8 @@ package tech.tolpuddle.faershaer_backend.http.dtos;
 
 import tech.tolpuddle.faershaer_backend.domain.Transaction;
 
-public record TransactionDto (String id, String payer, String expense, Double sum) {
+public record TransactionDto (String id, String portraitUrl, String payer, String expense, Double sum) {
     public static TransactionDto getDto(Transaction t) {
-        return new TransactionDto(t.getId(), t.getPayer().getName(), t.getExpense(), t.getSum());
+        return new TransactionDto(t.getId(), t.getPayer().getPortraitUrl(), t.getPayer().getName(), t.getExpense(), t.getSum());
     }
 }
